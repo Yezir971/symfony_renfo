87c47c0260ec3b4f4e7ae246be3c0530fffd622a
Yezir971 <james_ahmedaly@yahoo.com>
2025-04-01 13:38:27 +0200

    f

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+// cela permet de surcharger nos variable d'environnement
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'dotenv_overload' => true,
+];
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
